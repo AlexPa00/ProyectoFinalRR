18d1c7d18153caace6b3211f1a9592915578fa5f
AlexPa00 <nuve98@gmail.com>
2022-11-06 13:00:53 -0300

    cambios en el nivel 2

---
+++ Assets/Scripts/nextnevel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextnevel : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Premio4")
+        {
+            SceneManager.LoadScene("Level3");
+        }
+    }
+}
